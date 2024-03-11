41a46069c6573298e74d2a26c25f26bfa8c41e0a
Ansh Viswanathan <ansh.viswanathan@gmail.com>
2024-03-10 22:36:44 -0400

    Integrated Instagram widget

--- akpsi_website_frontend/src/components/Gallery.jsx
+++ akpsi_website_frontend/src/components/Gallery.jsx
@@ -1,72 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import "../components/Gallery.css"; // Make sure the path to your CSS file is correct
 
 const Gallery = () => {
-  const [loginStatusMessage, setLoginStatusMessage] = useState(
-    "Checking Facebook login status..."
-  );
-
-  useEffect(() => {
-    function statusChangeCallback(response) {
-      if (response.status === "connected") {
-        setLoginStatusMessage("You are logged into Facebook.");
-      } else if (response.status === "not_authorized") {
-        setLoginStatusMessage(
-          "You are logged into Facebook, but have not authorized the app."
-        );
-      } else {
-        setLoginStatusMessage("You are not logged into Facebook.");
-      }
-    }
-
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: "748944197199516", // Use your actual Facebook App ID
-        cookie: true,
-        xfbml: true,
-        version: "v12.0", // Use the actual API version
-      });
-
-      window.FB.AppEvents.logPageView();
-
-      window.FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-      });
-    };
-
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  }, []);
-
-  const handleLogin = () => {
-    window.FB.login(
-      function (response) {
-        if (response.authResponse) {
-          console.log("Welcome!  Fetching your information.... ");
-          window.FB.api("/me", function (response) {
-            console.log("Good to see you, " + response.name + ".");
-            setLoginStatusMessage(`Logged in as ${response.name}`);
-          });
-        } else {
-          console.log("User cancelled login or did not fully authorize.");
-        }
-      },
-      { scope: "public_profile" }
-    ); // Specify the permissions your app needs
-  };
-
   return (
-    <div>
-      <h1>Gallery</h1>
-      <p>This is the Gallery Page.</p>
-      <p>{loginStatusMessage}</p>
-      <button onClick={handleLogin}>Log in with Facebook</button>
+    <div className="gallery-page">
+      <div className="gallery-container">
+        {/* The title in an <h1> tag with the class "gallery-title" */}
+        <h1 className="gallery-title">GALLERY</h1>
+
+        {/* Include the introduction paragraph if you want it */}
+        <p className="gallery-intro">
+          Explore the visual journey of AKPsi OT through our Instagram feed.
+        </p>
+
+        {/* The Instagram section remains unchanged */}
+        <div className="instagram-section">
+          <iframe
+            src="//lightwidget.com/widgets/143bc4bf938d5d478ba2c2d547050754.html"
+            scrolling="yes"
+            allowtransparency="true"
+            style={{
+              width: "80%", // Adjust the width as needed
+              border: 0,
+              overflow: "hidden",
+              minHeight: "500px", // Adjust the height as needed
+              paddingTop: "15px",
+            }}
+            title="Instagram Feed"
+          />
+        </div>
+      </div>
     </div>
   );
 };
